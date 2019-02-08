2f31fc5bdc97df1311186c17ce27c07f175360ad
RobustSP <44571522+RobustSP@users.noreply.github.com>
2019-02-08 12:57:31 +0100

    Update whub.m

--- codes/07_AuxiliaryFunctions/whub.m
+++ codes/07_AuxiliaryFunctions/whub.m
@@ -2,4 +2,5 @@ function wx = whub(absx,c)
 % Huber's weight function w: input is N x 1 data vector x which can be complex
 % or real and threshold contant c
 wx = 1.*(absx<=c) + (c*(1./absx)).*(absx>c);
+wx(absx==0) = 1;
 end
